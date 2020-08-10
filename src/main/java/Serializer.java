ce2b818d4d7132ce9050b49472e6309be52e6f2d
Ivan Aboyan <iv.aboyan@gmail.com>
2020-08-10 03:59:03 +0300

    Update Serializer.java add writeInFile and rearFromFile methods

--- src/main/java/Serializer.java
+++ src/main/java/Serializer.java
@@ -25,5 +25,25 @@ public class Serializer {
         }
         return null;
     }
+
+    public static void writeInFile(byte[] array, String filePath) {
+        try (FileOutputStream fos = new FileOutputStream(filePath)) {
+            fos.write(array, 0, array.length);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static byte[] rearFromFile(String filePath) {
+        try (FileInputStream fin = new FileInputStream(filePath)) {
+            byte[] buffer = new byte[fin.available()];
+            fin.read(buffer, 0, fin.available());
+            return buffer;
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+        }
+        return null;
+    }
+
 }
 
